5ba0dcab6cf5d65c723c6d6dba66cca026549272
ppprac <70270759+ppprac@users.noreply.github.com>
2020-08-29 12:54:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup()
 {
   createCanvas(800,400);
  rect1 =  createSprite(400, 200, 50, 80);
- rect2 = createSprite(0,0, 80, 50)
+ rect2 = createSprite(100,200, 80, 50)
  rect1.shapeColor="green";
  rect2.shapeColor="green";
  rect1.debug=true;
@@ -13,7 +13,7 @@ function setup()
 
 function draw() 
 {
-  background(173,216,230);  
+  background(0,0,255);  
   rect2.x=World.mouseX;
   rect2.y=World.mouseY;
   if(rect2.x - rect1.x < rect2.width/2 + rect1.width/2 && rect1.x - rect2.x < rect2.width/2 + rect1.width/2)
